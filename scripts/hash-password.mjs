0fb95b0f30552339e4417ee33c3814931c2e563f
PINK <henghengrahman@gmail.com>
2026-09-16 14:47:03 +0700

    Delete scripts directory

--- scripts/hash-password.mjs
+++
@@ -1,1 +0,0 @@
-import {hashPassword} from '../src/auth.js'; const p=process.argv[2]; if(!p){console.error('Usage: node scripts/hash-password.mjs \"strong-password\"');process.exit(1)} console.log(hashPassword(p));
